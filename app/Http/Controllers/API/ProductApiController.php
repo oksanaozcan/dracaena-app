7f655303d903f8a2a5c358fa971465d8fd42c1cf
oksanaozcan <mekansa83@gmail.com>
2023-12-07 12:05:37 +0300

    ProductApiController refactoring

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -11,29 +11,53 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductApiController extends Controller
 {
-    public function index(Request $request): JsonResource
+    public function processRequest (Request $request)
     {
         $categoryId = $request->query('category_id');
         $tagId = $request->query('tag_id');
+        $search = $request->query('search');
 
         if ($categoryId) {
-            $products = Product::where('category_id', $categoryId)
-            // ->orderBy('price')
-            ->get();
+            return $this->getProductsByCategory($categoryId);
         }
-
         if ($tagId) {
-            $products = Product::whereHas('tags', function ($query) use ($tagId) {
-                $query->where('tag_id', $tagId);
-            })
-            // ->orderBy('price')
-            ->get();
+            return $this->getProductsByTag($tagId);
         }
-
-        if (!$tagId && !$categoryId) {
-            $products = Product::all();
+        if ($search) {
+            return $this->getProductsBySearch($search);
         }
+        if (!$tagId && !$categoryId && !$search) {
+            return $this->index($request);
+        }
+    }
 
+    public function getProductsByCategory($categoryId): JsonResource
+    {
+        $products = Product::where('category_id', $categoryId)
+        // ->orderBy('price')
+        ->get();
+        return ProductResource::collection($products);
+    }
+
+    public function getProductsByTag($tagId): JsonResource
+    {
+        $products = Product::whereHas('tags', function ($query) use ($tagId) {
+            $query->where('tag_id', $tagId);
+        })
+        // ->orderBy('price')
+        ->get();
+        return ProductResource::collection($products);
+    }
+
+    public function getProductsBySearch($search): JsonResource
+    {
+        $products = Product::where('title', 'like', '%'.$search.'%')->get();
+        return ProductResource::collection($products);
+    }
+
+    public function index(Request $request): JsonResource
+    {
+        $products = Product::all();
         return ProductResource::collection($products);
     }
 
